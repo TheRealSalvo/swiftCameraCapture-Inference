c90cb0e9bee217e9b9aa4dc8c6473272c77ade6b
TheRealSalvo <salva0729@gmail.com>
2024-03-06 14:44:51 +0100

    added project files

---
+++ swift+CameraCapture+Inference/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swift+CameraCapture+Inference
+//
+//  Created by Salvatore Attanasio on 06/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift+CameraCapture+Inference/swift_CameraCapture_InferenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_CameraCapture_InferenceApp.swift
+//  swift+CameraCapture+Inference
+//
+//  Created by Salvatore Attanasio on 06/03/24.
+//
+
+import SwiftUI
+
+@main
+struct swift_CameraCapture_InferenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
